52d5eb5279eed2cc3dd5c6fe42741ca27ede091b
Zain Ahmad Fahrezi <zainahmadfahrezi@gmail.com>
2025-08-08 13:09:59 +0700

    error upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <div className="bg-[#f0f7ff]">
-      <Header />
+      <Header />git
       <main>
         <Hero />
         <Problems />

0b1e9e12cd6aca47e0bb910a436b1682206d22cf
Zain Ahmad Fahrezi <zainahmadfahrezi@gmail.com>
2025-08-10 08:30:39 +0700

    bug2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
+import { authOptions as baseAuthOptions } from '@/lib/authOptions';
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  ...baseAuthOptions,
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
 export { handler as GET, handler as POST };
